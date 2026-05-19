43a637563b627a21ea3d72df58ca095e0348d4d4
taijarals <taijara@outlook.com.br>
2026-05-19 18:05:38 +0000

    changes

--- src/entities.c
+++
@@ -1,6 +0,0 @@
-#include "entities.h"
-
-void DrawCharacter(Character *c)
-{
-    DrawTexture(c->texture, c->rect.x, c->rect.y, WHITE);
-}
--- src/ui.c
+++
@@ -1,7 +0,0 @@
-#include "ui.h"
-#include "raylib.h"
-
-void DrawMenu()
-{
-    DrawText("LOBO, OVELHA E CACADOR", 320, 20, 40, YELLOW);
-}
